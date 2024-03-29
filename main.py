8a706b7faf0817b4abdf33edd43b8f3493083782
SurivZ <franklinserrano23@gmail.com>
2024-03-29 00:32:38 -0500

    Configuraciones iniciales

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, Response
+from PySQLiteDBConnection import Connect
+from json import dumps
+from os import environ
+
+app = Flask("Fun Seekers Tournament API")
+
+database = Connect("database.sqlite3")
+
+
+@app.route("/test")
+def test():
+    database.connect()
+    players = database.read_table("players_test")
+    data = {}
+    for player in players:
+        data[player[0]] = {
+            "summoner_name": player[1],
+            "level": player[2]
+        }
+    return Response(status=200, response=dumps(data), content_type="application/json")
+
+
+@app.route("/")
+def home():
+    database.connect()
+    players = database.read_table("players")
+    data = {}
+    for player in players:
+        data[player[0]] = {
+            "summoner_name": player[1],
+            "level": player[2]
+        }
+    return Response(status=200, response=dumps(data), content_type="application/json")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=1000)
